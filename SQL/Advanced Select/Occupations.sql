f6a599cbcc1db85f3db7880b518542ab3d942c19
Harini Sivakumar <63298200+Harry12901@users.noreply.github.com>
2022-06-25 21:43:58 +0530

    Update Occupations.sql

--- SQL/Advanced Select/Occupations.sql
+++ SQL/Advanced Select/Occupations.sql
@@ -45,4 +45,24 @@ The fourth column is an alphabetically ordered list of Actor names.
 The empty cell data for columns with less than the maximum number of names per occupation (in this case, the Professor and Actor columns) are filled with NULL values.
 
 */
-
+select
+    Doctor,
+    Professor,
+    Singer,
+    Actor
+from (
+    select
+        NameOrder,
+        max(case Occupation when 'Doctor' then Name end) as Doctor,
+        max(case Occupation when 'Professor' then Name end) as Professor,
+        max(case Occupation when 'Singer' then Name end) as Singer,
+        max(case Occupation when 'Actor' then Name end) as Actor
+    from (
+            select
+                Occupation,
+                Name,
+                row_number() over(partition by Occupation order by Name ASC) as NameOrder
+            from Occupations
+         ) as NameLists
+    group by NameOrder
+    ) as Names;
